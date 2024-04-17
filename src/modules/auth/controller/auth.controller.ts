b3ac47e9c2a9ac949e9cffeffab2b90724353a87
zakarya ghanai <zakariyaghanai@gmail.com>
2024-04-17 19:47:58 +0100

    add sign-up route

--- src/modules/auth/controller/auth.controller.ts
+++ src/modules/auth/controller/auth.controller.ts
@@ -1,20 +1,26 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from 'src/modules/user/service/user.service';
 import { SignInDto } from '../dto/sign-in.dto';
+import { SignUpDto } from '../dto/sign-up.dto';
 import { AuthenticationService } from '../service/authentication.service';
-
 
 @Controller('auth')
 export class AuthController {
 
   constructor(
-    private readonly authService: AuthenticationService
+    private readonly authService: AuthenticationService,
+    private readonly userService: UserService
   ) { }
 
   @Post('sign-in')
-  async signIn(@Body() signInDto: SignInDto) {
+  signIn(@Body() signInDto: SignInDto) {
     const { email, password, rememberMe } = signInDto
     return this.authService.signIn(email, password, rememberMe);
   }
 
+  @Post('sign-up')
+  singUp(@Body() signUpDto: SignUpDto) {
+    return this.userService.create(signUpDto);
+  }
 
 }
--- src/modules/auth/dto/sign-up.dto.ts
+++ src/modules/auth/dto/sign-up.dto.ts
@@ -1,6 +1,6 @@
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
-export class CreateUserDto {
+export class SignUpDto {
   @IsEmail()
   email: string;
 
