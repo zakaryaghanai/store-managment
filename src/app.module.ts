c635a8b5fef7a6628d80e2b61ada87d628fa0c3a
zakarya ghanai <zakariyaghanai@gmail.com>
2024-04-17 19:13:36 +0100

    add typeorm config and depnedencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { typeOrmConfig } from './common/database/typeorm.config';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".env",
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService) =>
+        typeOrmConfig(configService),
+      inject: [ConfigService],
+    }),
+    UserModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
